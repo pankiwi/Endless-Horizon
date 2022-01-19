b6393a74811930e08ce5054d286e97a94c07f3e1
pankiwi <arroliga.andrz1@gmail.com>
2022-01-19 09:49:48 -0600

    thx baba

--- src/endless/world/blocks/defense/MultiPallaraxBlock.java
+++ src/endless/world/blocks/defense/MultiPallaraxBlock.java
@@ -1,23 +1,14 @@
 package Endlss.world.blocks.defense;
 
-import arc.*;
-import arc.graphics.*;
-import arc.graphics.g2d.*;
-import arc.math.*;
-import arc.struct.*;
-import arc.util.*;
-import mindustry.content.*;
-import mindustry.entities.*;
-import mindustry.entities.bullet.*;
 import mindustry.gen.*;
-import mindustry.graphics.*;
-import mindustry.type.*;
-import mindustry.ctype.*;
-import mindustry.ui.*;
-import mindustry.world.*;
-import mindustry.world.blocks.*;
-import mindustry.world.meta.*;
+import mindustry.world.Block;
 
 public class MultiPallaraxBlock extends Block {
-  
+  public MultiPallaraxBlock(String name) {
+  	super(name);
+  }
+
+  public MultiPallaraxBlockBuild extends Building {
+  	
+  }
 }
